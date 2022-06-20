f69d9aefcd6c150ac65553bff8c15554d34cfe0d
Swan Herigault <swan.herigaultvaletudie@viacesi.fr>
2022-06-20 22:26:51 +0200

    Resolves vuetify and header

--- front/client/src/App.vue
+++ front/client/src/App.vue
@@ -2,12 +2,6 @@
   <v-app>
     <page-header />
     <v-main>
-      <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/register">Register</router-link> |
-        <router-link to="/login">Login</router-link>
-      </nav>
       <router-view/>
     </v-main>
   </v-app>
--- front/client/src/components/Header.vue
+++ front/client/src/components/Header.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-app id="inspire">
-    <!-- <v-navigation-drawer
+    <v-navigation-drawer
     v-model="drawer"
     app
     >
@@ -24,29 +23,20 @@
         <v-list-item
           v-for="item in items"
           :key="item.title"
+          :to="item.to"
           link
         >
-          <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer> -->
+    </v-navigation-drawer>
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
-
-    <v-main>
-      
-    </v-main>
-  </v-app>
 </template>
 
 <script lang="ts">
@@ -57,9 +47,11 @@ export default defineComponent({
   data: () => ({
     drawer: null, 
     items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'Photos', icon: 'mdi-image' },
-          { title: 'About', icon: 'mdi-help-box' },
+          { title: 'Home', icon: 'mdi-view-dashboard', to: '/' },
+          { title: 'About', icon: 'mdi-image', to: '/about' },
+          { title: 'Register', icon: 'mdi-help-box', to: '/register' },
+          { title: 'Login', icon: 'mdi-help-box', to: '/login' },
+
         ],
     }),
 });
