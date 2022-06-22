aef54917188119bbf5635a2bb59f4651bac6228c
Jules-CLERC <jules.clerc0711@gmail.com>
2022-06-22 11:53:22 +0200

    Features RestaurantController
    - Select attributes send to the front (for example we don't want to send the password)
    - Creation of get restaurant by Id
    
    TODO : Send products to the front in getRestaurant()

--- server/controllers/RestaurantController.ts
+++ server/controllers/RestaurantController.ts
@@ -1,19 +1,54 @@
 const userModelRestaurants = require("../models/User.ts");
+const mongoose = require('mongoose');
 
 module.exports = {
     //get all restaurants availables
-    //TODO : add filter to select the restaurants users
     async getAll(req, res) {
-        userModelRestaurants.find({role: 'restorer'}).then(function(restaurants) {
-            try {
-                res.send({
-                    restaurants: restaurants
-                });
-            } catch (err) {
-                res.status(400).send({
-                    error: 'Impossible to get the restaurants.'
-                })
-            }
+        userModelRestaurants
+            .find({role: 'restorer', status: 'active'})
+            .select('username category description schedule adress')
+            .then(function(restaurants) {
+                try {
+                    res.send({
+                        restaurants: restaurants
+                    });
+                } catch (err) {
+                    res.status(400).send({
+                        error: 'Impossible to get the restaurants.'
+                    })
+                }
         });
+    },
+    //get restaurant by Id
+    async getRestaurant(req, res) {
+        if(mongoose.Types.ObjectId.isValid(req.params.id)) {
+            userModelRestaurants
+                .findById(req.params.id)
+                .select('username category description schedule adress status role')
+                .then(function(restaurant) {
+                    try {
+                        if(restaurant.role !== 'restorer' || restaurant.status !== 'active') {
+                            res.status(400).send({
+                                error: "This restaurant is not available."
+                            });
+                        }
+                        else {
+                            res.send({
+                                restaurant: restaurant
+                                //TODO: send also the products of the restaurant
+                            });
+                        }
+                    } catch (err) {
+                        res.status(400).send({
+                            error: 'Impossible to get the restaurant.'
+                        })
+                    }
+            });
+        }
+        else {
+            res.status(404).send({
+                error: 'Restaurant not found.'
+            })
+        }
     }
 }
--- server/routes.ts
+++ server/routes.ts
@@ -12,4 +12,7 @@ module.exports = (app) => {
 
   app.get('/restaurants',
     RestaurantController.getAll)
+
+  app.get('/restaurants/:id',
+    RestaurantController.getRestaurant)
 }
