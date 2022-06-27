b6c3bdc0aef951865875d1558672c85f165e8e08
Jules-CLERC <jules.clerc0711@gmail.com>
2022-06-27 11:07:39 +0200

    Add profile picture

--- server/models/User.ts
+++ server/models/User.ts
@@ -25,8 +25,8 @@ const UserSchema = new mongoose.Schema(
         required: [true, 'name field is required'],
       },
       profile_picture: {
-        data: Buffer,
-        contentType: String
+        type: String,
+        required: false
       },
       email: {
         type: String,
