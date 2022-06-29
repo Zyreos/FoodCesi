02a48745e450d5e756de754ab42a83b2be06a69d
Swan Herigault <swan.herigaultvaletudie@viacesi.fr>
2022-06-29 19:28:54 +0200

    List order view for deliverer

--- front/deliverer/src/components/ListOrdersComponents.vue
+++ front/deliverer/src/components/ListOrdersComponents.vue
@@ -1,72 +1,97 @@
 <template>
-    <h1>List orders</h1>
+    <div class="title">
+        <h1>List orders</h1>
+    </div>
+
     <div class="error" v-if="error">
         {{ error }}
     </div>
-    <h2>To Deliver</h2>
-    <div id="container-list-orders" v-if="ordersDelivering">
-
-        <v-card class="mx-6 my-12" width="250" v-for="order in getOrdersWithoutDeliverer()">
-
-            <v-card-title>
-                Address : {{ order.address }}
-            </v-card-title>
-
-            <v-card-actions>
-                <v-btn icon @click="show1 = !show1">
-                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                </v-btn>
-                <v-spacer></v-spacer>
-
-                <v-btn @click="confirmDelivererOrder(order)">
-                    <v-icon color="secondary">
-                        mdi-check
-                    </v-icon>
-                </v-btn>
-            </v-card-actions>
-            <v-expand-transition>
-                <div v-show="show1">
-
-                    <v-card-subtitle class="my-2" v-for="article in order.content">
-                        {{ article }}
-                    </v-card-subtitle>
-
-                </div>
-            </v-expand-transition>
-
-        </v-card>
+    <div class="subtitle">
+        <h2>To Deliver</h2>
     </div>
-    <h2>Delivered</h2>
+    <v-divider class="divider"></v-divider>
     <div id="container-list-orders" v-if="ordersDelivering">
-        <v-card class="mx-6 my-12" width="250" v-for="order in getOrdersToDelivered()">
-            <v-card-title>
-                Total price : {{ order.price.total_price }}$
-            </v-card-title>
+        <v-row class="test">
+            <v-col cols="12" sm="3">
+                <v-card class="mx-6 my-12" width="300" v-for="order in getOrdersWithoutDeliverer()">
 
-            <v-card-actions>
-                <v-btn icon @click="show2 = !show2">
-                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                </v-btn>
-                <v-spacer></v-spacer>
+                    <v-card-title>
+                        Order : {{ order._id.slice(19, 24) }}
+                    </v-card-title>
 
-                <v-btn @click="confirmDeliveredOrder(order)">
-                    <v-icon color="secondary">
-                        mdi-check
-                    </v-icon>
-                </v-btn>
-            </v-card-actions>
+                    <v-card-title>
+                        Address : {{ order.address }}
+                    </v-card-title>
 
-            <v-expand-transition>
-                <div v-show="show2">
+                    <v-card-actions>
+                        <v-btn icon @click="show1 = !show1">
+                            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                        <v-spacer></v-spacer>
 
-                    <v-card-subtitle class="my-2" v-for="article in order.content">
-                        {{ article }}
-                    </v-card-subtitle>
+                        <v-btn @click="confirmDelivererOrder(order)">
+                            <v-icon color="secondary">
+                                mdi-check
+                            </v-icon>
+                        </v-btn>
+                    </v-card-actions>
+                    <v-expand-transition>
+                        <div v-show="show1">
 
-                </div>
-            </v-expand-transition>
-        </v-card>
+                            <v-card-subtitle class="my-2" v-for="article in order.content">
+                                {{ article }}
+                            </v-card-subtitle>
+
+                        </div>
+                    </v-expand-transition>
+
+                </v-card>
+            </v-col>
+        </v-row>
     </div>
+    <v-divider class="divider"></v-divider>
+    <div class="subtitle">
+        <h2>Delivered</h2>
+    </div>
+    <v-divider class="divider"></v-divider>
+    <div id="container-list-orders" v-if="ordersDelivering">
+        <v-row class="test">
+            <v-col cols="12" sm="3">
+                <v-card class="mx-6 my-12" width="300" v-for="order in getOrdersToDelivered()">
+                    <v-card-title>
+                        Order : {{ order._id.slice(19, 24) }}
+                    </v-card-title>
+                    <v-card-title>
+                        User Code : {{ order.users.user_id.slice(19, 24) }}
+                    </v-card-title>
+
+                    <v-card-actions>
+                        <v-btn icon @click="show2 = !show2">
+                            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                        <v-spacer></v-spacer>
+
+                        <v-btn @click="confirmDeliveredOrder(order)">
+                            <v-icon color="secondary">
+                                mdi-check
+                            </v-icon>
+                        </v-btn>
+                    </v-card-actions>
+
+                    <v-expand-transition>
+                        <div v-show="show2">
+
+                            <v-card-subtitle class="my-2" v-for="article in order.content">
+                                {{ article }}
+                            </v-card-subtitle>
+
+                        </div>
+                    </v-expand-transition>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+    <v-divider class="divider"></v-divider>
 </template>
 
 <script>
@@ -91,22 +116,22 @@ export default defineComponent({
             return tmpOrders;
         },
         async confirmDelivererOrder(order) {
-            const response = await OrderService.confirmDelivererOrder(order._id, this.$store.state.user._id, {users: order.users});
-            if(response.status == 200){
+            const response = await OrderService.confirmDelivererOrder(order._id, this.$store.state.user._id, { users: order.users });
+            if (response.status == 200) {
                 console.log(response);
                 //reload page
                 this.ordersDelivering = (await OrderService.getOrdersDelivering()).data.orders;
-            } 
+            }
         },
         async confirmDeliveredOrder(order) {
             const response = await OrderService.changeOrderState(order._id, {
                 state: "Delivered"
             });
-            if(response.status == 200){
+            if (response.status == 200) {
                 console.log(response);
                 //reload page
                 this.ordersDelivering = (await OrderService.getOrdersDelivering()).data.orders;
-            } 
+            }
         }
     },
     async mounted() {
@@ -119,3 +144,36 @@ export default defineComponent({
     },
 });
 </script>
+
+<style>
+.title {
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    text-align: center;
+    text-decoration: underline;
+    margin-bottom: 20px;
+}
+
+.articles {
+    display: block;
+}
+
+.subtitle {
+    margin-left: 20px;
+    text-decoration: underline;
+}
+
+.divider {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+#container-list-orders {
+    width: 100%;
+}
+
+.test {
+    justify-content: space-around;
+}
+</style>
