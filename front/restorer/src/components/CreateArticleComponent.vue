63dc4bfbad318d7adc9b4775732fb6283eb6c24f
Jules-CLERC <jules.clerc0711@gmail.com>
2022-06-29 11:56:43 +0200

    Create page Article (back end)

--- front/restorer/src/components/CreateArticleComponent.vue
+++ front/restorer/src/components/CreateArticleComponent.vue
@@ -1,167 +1,120 @@
 <template>
- <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation 
-    class="mx-12 my-12"
-  >
-    <v-text-field
-      v-model="name"
-      :counter="30"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
+  <v-form ref="form" lazy-validation class="mx-12 my-12">
+    <v-text-field v-model="name" :counter="30" label="Name" required></v-text-field>
 
-    <v-text-field
-      v-model="category"
-      :counter="30"
-      :rules="nameRules"
-      label="Category"
-      required
-    ></v-text-field>
+    <v-text-field v-model="category" :counter="30" :rules="nameRules" label="Category" required></v-text-field>
 
-    <v-text-field
-      v-model="price"
-      type="number"
-      label="Price"
-      required
-    ></v-text-field>
+    <v-text-field v-model="price" type="number" label="Price" required></v-text-field>
 
-    <v-text-field
-      v-model="description"
-      :counter="50"
-      :rules="nameRules"
-      label="Description"
-      required
-    ></v-text-field>
+    <v-text-field v-model="description" :counter="50" label="Description" required></v-text-field>
 
 
-    <v-text-field
-      v-model="picture"
-      :counter="50"
-      :rules="nameRules"
-      label="URL-picture"
-      required
-    ></v-text-field>
+    <v-text-field v-model="picture" :counter="50" label="URL-picture" required></v-text-field>
+    <v-img :src="picture" height="200px" style="border: 1px solid black;"></v-img>
+    <br/>
+    <v-text-field v-model="available_quantity" type="number" label="Available quantity" required></v-text-field>
 
-    <v-text-field
-      v-model="available_quantity"
-      type="number"
-      label="Available quantity"
-      required
-    ></v-text-field>
+    <v-col>
+      <v-container class="px-0" fluid>
+        <v-switch v-model="show" label="Menu" color="blue"></v-switch>
+      </v-container>
+      <div v-show="show">
+        <v-select
+          v-model="products"
+          :items="itemsArticles"
+          item-title="name"
+          item-value="_id"
+          label="Articles"
+          multiple
+          hint="Pick your articles"
+          persistent-hint
+        ></v-select>
+      </div>
+    </v-col>
 
-  <v-col>
-    <v-switch
-     @click="show = !show"
-      v-model="showAddArticles"
-      label="Menu"
-      color="blue"
-    ></v-switch>
-    
-            <div v-show="show">
-            <v-card-actions>
-                <v-card-title>Add Articles</v-card-title>
-                <v-spacer></v-spacer> 
-                <v-btn @click="show1 = !show1">
-                    <v-icon>
-                        mdi-plus
-                    </v-icon>
-                </v-btn>
-            </v-card-actions>
-            <div v-show="show1">
-                <v-text-field
-                v-model="article_name"
-                :counter="30"
-                :rules="nameRules"
-                label="Name"
-                required
-                ></v-text-field>
+    <div class="error" v-html="error" />
+    <br />
 
-                <v-text-field
-                    v-model="article_category"
-                    :counter="30"
-                    :rules="nameRules"
-                    label="Category"
-                     required
-                ></v-text-field>
-
-
-                <v-text-field
-                    v-model="article_description"
-                    :counter="50"
-                    :rules="nameRules"
-                    label="Description"
-                    required
-                ></v-text-field>
-                <v-card-actions> 
-                     <v-spacer></v-spacer>               
-                    <v-btn >
-                    Add
-                </v-btn>
-                
-            </v-card-actions>
-
-         </div>        
-            </div>
-   
-
-
-  </v-col>
-<v-card-actions>
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="validate"
-    >
-      Validate
-    </v-btn>
-    <v-spacer></v-spacer>  
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
-    <v-spacer></v-spacer>  
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
-</v-card-actions>
+    <v-card-actions>
+      <v-btn color="success" class="mr-4" @click="validate">
+        Validate
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="error" class="mr-4" @click="reset">
+        Reset Form
+      </v-btn>
+    </v-card-actions>
   </v-form>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
+import ArticleService from "../../../global/services/ArticleService";
+export default {
+  data: () => ({
+    show: false,
+    error: null,
+    name: null,
+    category: null,
+    price: 0,
+    description: null,
+    picture: null,
+    available_quantity: null,
+    products: [],
+    itemsArticles: []
+  }),
 
-      
-      show: false,
-      show1: false,
-    }),
-
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
+  methods: {
+    async validate() {
+      if(this.show === false) {
+        this.products = [];
+      }
+      //create article
+      try {
+        const response = await ArticleService.newArticle({
+          restaurant: this.$store.state.user._id,
+          name: this.name,
+          category: this.category,
+          price: this.price,
+          description: this.description,
+          picture: this.picture,
+          available_quantity: this.available_quantity,
+          products: this.products
+        });
+        if(response.status == 200){
+          //reload page
+          console.log(response);
+          this.reset();
+        } 
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     },
-  }
+    reset() {
+      this.$refs.form.reset()
+    },
+  },
+  async mounted() {
+    //get all articles from the restaurant
+    try {
+      const productsRestaurant = (await ArticleService.getArticlesRestaurant(this.$store.state.user._id)).data.articles;
+      this.itemsArticles = productsRestaurant.filter(article => article.products.length == 0);
+      console.log(this.itemsArticles);
+    } catch (err) {
+      this.error = err.message;
+    }
+
+            // try {
+            //     this.orders = (await OrderService.getOrdersUser(this.$store.state.user._id)).data.orders;
+            //     console.log(this.orders);
+            // } catch (err) {
+            //     this.error = err.message;
+            // }
+  },
+}
 </script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
